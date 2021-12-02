e1cf5b0952150fc1e991952a172d7d5df4853b86
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-12-02 14:19:50 +0330

    v08 is completed

--- echo/consumers.py
+++ echo/consumers.py
@@ -25,36 +25,34 @@ class ChatConsumer(WebsocketConsumer):
 		self.user_id = self.scope['url_route']['kwargs']['username']
 		self.group_name = f"chat_{self.user_id}"
 		
-		async_to_sync(self.channel_layer.group_add(
+		async_to_sync(self.channel_layer.group_add)(
 			self.group_name,
 			self.channel_name
-		))
+		)
 
 
 		self.accept()
 
 
 	def disconnect(self, close_code):
-		async_to_sync(self.channel_layer.group_discard(
+		async_to_sync(self.channel_layer.group_discard)(
 			self.group_name,
 			self.channel_name
-		))
+		)
 
 	def receive(self, text_data=None, bytes_data=None):
 		if text_data:
 			text_data_json = json.loads(text_data)
-			username = text_data_json['username']
+			username = text_data_json['receiver']
 			user_group_name = f"chat_{username}"
 
-			async_to_sync(
-				self.channel_layer.group_send(
-					user_group_name,
+			async_to_sync(self.channel_layer.group_send)(
+				user_group_name,
 					{
 						'type': 'chat_message',
 						'message': text_data
 					}
 				)
-			)
 
 	def chat_message(self, event):
 		message = event['message']
