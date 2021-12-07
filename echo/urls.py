55a2a53e669d865f456f88f270b341d92a971c9e
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-12-07 15:05:23 +0330

    v11 is watched

--- echo/urls.py
+++ echo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, echo_image, join_chat
+from .views import index, echo_image, join_chat, new_message
 
 
 urlpatterns = [
 	path('', index, name='index'),
 	path('image/', echo_image, name='echo_image'),
-	path('chat/<str:username>', join_chat, name='join_chat')
+	path('chat/<str:username>', join_chat, name='join_chat'),
+	path('chat/new/<str:username>', new_message, name='new_message')
 ] 
--- echo/views.py
+++ echo/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, HttpResponse
-from django.utils.safestring import mark_safe
 import json
+from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+from django.utils.safestring import mark_safe
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 # Create your views here.
 def index(request):
@@ -11,3 +14,21 @@ def echo_image(request):
 
 def join_chat(request, username):
 	return render(request, 'echo/join_chat.html', {'username_json': mark_safe(json.dumps(username))})
+
+def new_message(request, username):
+	reciver = request.GET['receiver']
+	text = request.GET['text']
+	channel_layer = get_channel_layer()
+	group_name = f'chat_{reciver}'
+	async_to_sync(channel_layer.group_send)(
+		group_name,
+		{
+			'type': 'chat_message',
+			'message': json.dumps({'sender': username, 'reciver': reciver, 'text': text})
+		}
+	)
+
+	return JsonResponse({
+		'message': json.dumps({'sender': username, 'reciver': reciver, 'text': text}),
+		'status': '200'
+	})
